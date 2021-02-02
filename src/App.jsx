ec9cb982ded143e524a6478d22df8eeb0c5275f6
coffmanjrp <coffmanjrp@gmail.com>
2021-02-01 21:18:32 -0800

    implement search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from './components/navbar/Navbar';
+import Search from './components/search/Search';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Search />
     </div>
   );
 }
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const Search = () => {
+  const [searchText, setSearchText] = useState('');
+  const [amount, setAmount] = useState(15);
+  const [apiUrl, setApiUrl] = useState('https://pixabay.com/api');
+  const [apiKey, setApiKey] = useState(process.env.REACT_APP_API_KEY);
+  const [images, setImages] = useState([]);
+
+  const selectOptions = [5, 10, 15, 30, 50];
+
+  const onTextChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const onAmountChange = () => {};
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `${apiUrl}/?key=${apiKey}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`
+      );
+      const data = await res.data.hits;
+
+      setImages(data);
+    };
+
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [searchText]);
+
+  return (
+    <div>
+      <TextField
+        name="searchText"
+        fullWidth={true}
+        label="Search For Images"
+        value={searchText}
+        onChange={onTextChange}
+      />
+      <br />
+      <TextField
+        name="amount"
+        select
+        fullWidth={true}
+        label="Amount"
+        value={amount}
+        onChange={onAmountChange}
+      >
+        {selectOptions.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </TextField>
+    </div>
+  );
+};
+
+export default Search;
